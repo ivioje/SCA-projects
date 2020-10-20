f92916f7446a9187edc1f7596ed49daa2ac3e34b
ivioje <rhodaivioje@gmail.com>
2020-10-20 14:50:39 +0100

    Task 1

---
+++ index.js
@@ -0,0 +1,10 @@
+let details = {
+  name: 'Eboreime Rhoda',
+  country: 'Nigeria',
+  field: 'Front end'
+}
+function myDetails () {
+  return `My name is ${details.name} from ${details.country} and I'm a ${details.field} developer.`
+}
+
+myDetails()
